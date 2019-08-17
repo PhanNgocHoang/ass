5fe45646dc4221c73b86d52b6d4317bccc40ea3e
PhanNgocHoang <52957470+PhanNgocHoang@users.noreply.github.com>
2019-08-17 20:23:26 +0700

    Update login.php

--- login.php
+++ login.php
@@ -2,12 +2,12 @@
     session_start();
     include_once "connect.php";
 
-    $sql = "SELECT * FROM login where username = :username and pass = :pass";
+    $sql = "SELECT * FROM store where usernamestore = :usernamestore and pass = :pass";
     $stmt = $pdo->prepare($sql);
     $stmt->execute
     (
         array(
-            'username' => $_POST['username'],
+            'usernamestore' => $_POST['username'],
             'pass' => $_POST['pass'],
         )
     );
@@ -20,7 +20,28 @@
     }
     else
     {
-        echo "<h1>Sai username or passs</h1>";
+        echo "<h1>Sai username or pass</h1>";
+    }
+
+    $sql = "SELECT * FROM manage where username = :username and pass = :pass";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute
+    (
+        array(
+            'username' => $_POST['username'],
+            'pass' => $_POST['pass'],
+        )
+    );
+    $count = $stmt->rowCount();
+    if($count > 0)
+    {   
+        $_SESSION['username'] = $_POST['username'];
+        header("Location: capcao.php");
+        
+    }
+    else
+    {
+        echo "<h1>Sai username or pass</h1>";
     }
 
         
